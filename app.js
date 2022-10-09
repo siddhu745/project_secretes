c813302ee723288459e2b3a8b0ffbd8409ae7bf7
siddhu745 <sivaramsiddhu@gmail.com>
2022-10-09 19:48:26 +0530

    level 3 security added md5 hashing module

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
 
 const app = express()
 
@@ -15,8 +16,6 @@ const userSchema = new mongoose.Schema({
     password : String
 })
 
-
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:['password']})
 
 
 const User = new mongoose.model("User",userSchema)
@@ -39,7 +38,7 @@ app.get('/register',function(req,res){
 app.post('/register',function(req,res){
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
@@ -57,8 +56,8 @@ app.get('/login',function(req,res){
 
 app.post('/login',function(req,res){
     const username = req.body.username
-    const password = req.body.password
-
+    const password = md5(req.body.password)
+ 
     User.findOne({email : username},function(err,foundUser){
         if(err){
             console.log(err);
@@ -90,3 +89,5 @@ app.post('/login',function(req,res){
 app.listen(745,function(){
     console.log("sever is running on port 745");
 })
+
+console.log(md5("123456"));
